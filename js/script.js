9792286ba0d2c3994f422ee19e21724d78f63d6a
simona <palumbosimona118@gmail.com>
2020-02-04 10:59:17 +0100

    commit finale

--- js/script.js
+++ js/script.js
@@ -13,19 +13,30 @@
 
 $(document).ready(function() {
 
-  var calendario = [];
-//effettuo la chiamata
-
+  for (var i = 1; i <= 31; i++) {
+    console.log() ;
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+    var context = {
+      date: moment(i + '/01/2018', 'D/MM/YYYY', true).format('D MMMM'),
+    };
+    console.log(context.date);
+    var html = template(context);
+    $('.lista-giorni').append(html);
+  }
   $.ajax({
-    url : "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
-    method : "GET",
-    success : function(dati, status) {
-      calendario = dati.response;
-      //console.log(dati);
-      console.log(calendario);
-    },
-    'error': function (request, state, errors) {
-      alert('error' + errors);
+       url : "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
+       method : "GET",
+       success : function(dati) {
+       console.log(dati.response[0].date);
+      for (var i = 0; i < dati.response.length; i++) {
+      var DataDaConsiderare = moment(dati.response[i].date, 'YYYY-MM-DD', true).format('D MMMM');
+      console.log(DataDaConsiderare);
+      for (var key = 0; k <= 31; k++) {
+        if (DataDaConsiderare == $('.lista-giorni li').eq(key).text()) {
+          $('.lista-giorni li').eq(key).append(' - ' + dati.response[i].name);
+          $('.lista-giorni li').eq(key).addClass('red');
+        }
+      }
     }
-  });
 });
